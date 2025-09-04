161a3bf6a3fe3a0dc83e03f7a9f3aff988892ef9
Cindy <mannieshae225@gmail.com>
2025-09-04 15:33:21 -0400

    n

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,5 +1,17 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Next.js Dashboard',
+    default: 'Next.js Dashboard',
+  },
+  description: 'A dashboard built with App Router, NextAuth, and PostgreSQL',
+  metadataBase: new URL('nextjs-dashboard-7vbh2eqtk-cynthia-daniels-projects.vercel.app')
+}
+
 export default function RootLayout({
   children,
 }: {
